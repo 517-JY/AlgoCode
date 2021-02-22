0181a791a6f64175a1080a785c90875d3651e1ef
Jiayin Li <64394513+517-JY@users.noreply.github.com>
2021-02-21 23:46:30 -0500

    Add files via upload
    Arrays - Easy - NonConstructibleChange 
    1. Sort 
    2. Find the rule (with/without the newly added item)

---
+++ Arrays/NonConstructibleChange.java
@@ -0,0 +1,24 @@
+public class NonConstructibleChange {
+	public int NonConstructibleChangeMethod(int[] coins) {
+		// sort array in asceding order
+		Arrays.sort(coins);
+
+		// a variable tracks how many combinations can be made so far
+		int numberOfCombinations = 0;
+
+		for(int coin : coins) {
+			// e.g. [1, 1, 2, 3, 5, 7, 22]
+			// before traversing 22, the numberOfCombinations is 19 (have a try brute-force)
+			// so 20 is the answer
+			if (coin > numberOfCombinations + 1) {
+				return numberOfCombinations + 1;
+			} else {
+				// two scaniaros : whether included the newly one item or not 
+				// with additional substracting the duplciated ones 
+				numberOfCombinations += coin;
+			}
+		}
+
+		return numberOfCombinations + 1;
+	}
+}
